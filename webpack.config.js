5f5eb467e4988f4b0b8bb93cb3853f0cce4fc01e
YermakA <ermak.080@mail.ru>
2023-06-11 17:22:36 +0800

    webpack configure was made

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/app.js'),
+  },
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    port: 3000
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+      filename: 'index.html',
+    }),
+
+    new CleanWebpackPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
