ab13ebca7be854e1ecde5f74a812c0715278c23d
YermakA <ermak.080@mail.ru>
2023-06-12 21:04:02 +0800

    modal window has been created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import './styles.css';
-import { isValid } from './utils';
+import { isValid, createModal } from './utils';
 import { Question } from './question.js'
 
 const form = document.getElementById('form');
+const modalBtn = document.getElementById('modal-btn');
 const input = form.querySelector('#questions-input');
 const sumbitBtn = form.querySelector('#submit');
 
+window.addEventListener('load', Question.renderList);
+modalBtn.addEventListener('click', openModal);
 form.addEventListener('submit', submitFormHandler);
 input.addEventListener('input', () => {
   sumbitBtn.disabled = !isValid(input.value);
@@ -33,4 +36,8 @@ function submitFormHandler(event) {
 
     console.log("Question", question);
   }
+}
+
+function openModal() {
+  createModal('Авторизация', `<h1>Test</h1>`);
 }
